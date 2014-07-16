df0ccb20b7faf2b7f2806acefa72e22b935bd433
ucym <ucym@users.noreply.github.com>
2014-07-16 21:10:48 +0900

    Roose_Input::serverメソッドが追加されました。
    $_SERVER変数に安全にアクセスできます。

--- classes/input.php
+++ classes/input.php
@@ -8,7 +8,7 @@
  * @version 1.0.0
  */ 
 class Roose_Input {
-    
+
     /**
      * リクエストメソッドの種類を取得します。
      * （POST, GET, etcetc...)
@@ -18,28 +18,43 @@ class Roose_Input {
     {
         return $_SERVER['REQUEST_METHOD'];
     }
-    
+
+
     /**
      * POSTパラメータの値を取得します。
      * 
-     * @param string|null $key 取得するパラメータ名
-     * @param mixed|null $default パラメータが存在しない時のデフォルト値
+     * @param string|null $key (optional) 取得するパラメータ名
+     * @param mixed|null $default (optional) パラメータが存在しない時のデフォルト値
      * @return mixed
      */ 
     public static function post($key = null, $default = null)
     {
         return Roose_Arr::get($_POST, $key, $default);
     }
-    
+
+
     /**
      * GETパラメータの値を取得します。
      * 
-     * @param string|null $key 取得するパラメータ名
-     * @param mixed|null $default パラメータが存在しない時のデフォルト値
+     * @param string|null $key (optional) 取得するパラメータ名
+     * @param mixed|null $default (optional) パラメータが存在しない時のデフォルト値
      * @return mixed
      */ 
     public static function get($key = null, $default = null)
     {
         return Roose_Arr::get($_GET, $key, $default);
+    }
+
+
+    /**
+     * $_SERVER変数の値を取得します。
+     *
+     * @param string|null $key (optional) 取得する属性名
+     * @param mixed|null $default (optional)パラメータが存在しない時のデフォルト値
+     * @return mixed
+     */
+    public static function server($key = null, $default = null)
+    {
+        return Roose_Arr::get($_SERVER, $key, $default);
     }
 }
