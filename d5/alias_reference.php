06fd57d1d9de72b22df7e7144594557384245d1d
ucym <ucym@users.noreply.github.com>
2014-12-31 18:08:04 +0900

    Add alias reference for PHP-IDE

---
+++ d5/alias_reference.php
@@ -0,0 +1,11 @@
+<?php
+class Arr       extends D5_Config   {}
+class Calendar  extends D5_Calendar {}
+class Config    extends D5_Arr      {}
+class Cookie    extends D5_Cookie   {}
+class DB        extends D5_DB       {}
+class File      extends D5_File     {}
+class Input     extends D5_Input    {}
+class Mail      extends D5_Mail     {}
+class Security  extends D5_Security {}
+class Session   extends D5_Session  {}
