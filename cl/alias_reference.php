4b05e3858bc0f16ee7741d7d89c00a422a7c8014
ucym <ucym@users.noreply.github.com>
2015-10-31 15:08:47 +0900

    Update IDE completion reference

--- cl/alias_reference.php
+++ cl/alias_reference.php
@@ -1,11 +1,16 @@
 <?php
-class Arr       extends \CodeLapse\Arr      {}
-class Calendar  extends \CodeLapse\Calendar {}
-class Config    extends \CodeLapse\Config   {}
-class Cookie    extends \CodeLapse\Cookie   {}
-class DB        extends \CodeLapse\DB       {}
-class File      extends \CodeLapse\File     {}
-class Input     extends \CodeLapse\Input    {}
-class Mail      extends \CodeLapse\Mail     {}
-class Security  extends \CodeLapse\Security {}
-class Session   extends \CodeLapse\Session  {}
+class Arr extends CodeLapse\Arr {}
+class Calendar extends CodeLapse\Calendar {}
+class Config extends CodeLapse\Config {}
+class Cookie extends CodeLapse\Cookie {}
+class DB extends CodeLapse\DB {}
+class File extends CodeLapse\File {}
+class Form extends CodeLapse\Form {}
+class Req extends CodeLapse\Request {}
+class Mail extends CodeLapse\Mail {}
+class Pager extends CodeLapse\Pager {}
+class Security extends CodeLapse\Security {}
+class Session extends CodeLapse\Session {}
+class RestController extends CodeLapse\Controller\Rest {}
+class Upload extends CodeLapse\Upload {}
+class Validator extends CodeLapse\Validato {}
