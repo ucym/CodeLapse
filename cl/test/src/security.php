b7d288fa319e8f2257d608d1eb0778afb8d4c0b4
thiry <thiry@five-seven.net>
2015-10-31 11:23:24 +0900

    added tests for Security class

---
+++ cl/test/src/security.php
@@ -0,0 +1,24 @@
+<?php
+use \CodeLapse\Security;
+
+class SecurityTest extends PHPUnit_Framework_TestCase
+{
+    public function testSafeHtml()
+    {
+        // doublequote and ampersand
+        $output = Security::safeHtml('"H&M"');
+        $expected = '&quot;H&amp;M&quot;';
+        $this->assertEquals($output, $expected);
+
+        // singlequote
+        $output = Security::safeHtml("'");
+        $expected = array('&#039;', '&apos;');
+        $this->assertContains($output, $expected);
+
+
+        // no double encode
+        $output = Security::safeHtml('You must write & as &amp;');
+        $expected = 'You must write &amp; as &amp;';
+        $this->assertEquals($output, $expected);
+    }
+}
